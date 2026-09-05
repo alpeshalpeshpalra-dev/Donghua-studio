11abf1492a4cd120f45fca49c25449dc28422ab8
alpeshalpeshpalra-dev <alpeshalpeshpalra@gmail.com>
2026-09-05 10:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,4 +191,9 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=10000)
   
-app = demo.app
+app = demo.app# Render Launch Code
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
+    
